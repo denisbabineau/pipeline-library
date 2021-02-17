197b9a59dfb90f17333fa39ac09ed0c1f6b3c781
Denis Babineau <denis.babineau@gmail.com>
2021-02-17 11:41:17 -0400

    disable windows platform

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -318,7 +318,7 @@ static List<Map<String, String>> recommendedConfigurations() {
         [ platform: "linux", jdk: "8", jenkins: null ],
         // [ platform: "windows", jdk: "8", jenkins: null ],
         // [ platform: "linux", jdk: "8", jenkins: recentLTS, javaLevel: "8" ],
-        [ platform: "windows", jdk: "8", jenkins: recentLTS, javaLevel: "8" ],
+        // [ platform: "windows", jdk: "8", jenkins: recentLTS, javaLevel: "8" ],
         // [ platform: "linux", jdk: "11", jenkins: recentLTS, javaLevel: "8" ],
         // [ platform: "windows", jdk: "11", jenkins: recentLTS, javaLevel: "8" ]
     ]
